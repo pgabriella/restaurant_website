9709a2456bb1a1e9f8045b6ccb838822adc40db8
pgabriella <81381710+pgabriella@users.noreply.github.com>
2021-09-21 17:51:57 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,87 @@
+function openTab(evt, tabName) {
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+      tabcontent[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+      tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+    document.getElementById(tabName).style.display = "block";
+    evt.currentTarget.className += " active";
+}
+
+var slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}    
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";  
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " active";
+}
+
+function myFunction() {
+    var x = document.getElementById("home");
+    
+    if (x.style.display === "none") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "none";
+    }
+}
+
+function validate(){
+    let name = document.getElementById("name").value;
+    let num = document.getElementById("num").value; 
+    let text = document.getElementById("text");  
+
+    if(name == 'rose' && num >= 1 && num <= 6 ){
+        text.innerHTML = 'your table is ready'
+    }else {
+        text.innerHTML = 'Something went wrong'
+        
+    }
+    text.style.color = 'orange';
+}
+
+var modal = document.getElementById('myModal');
+var images = document.getElementsByClassName('myImages');
+var modalImg = document.getElementById("img01");
+var captionText = document.getElementById("caption");
+
+for (var i = 0; i < images.length; i++) {
+  var img = images[i];
+  img.onclick = function() {
+    modal.style.display = "block";
+    modalImg.src = this.src;
+    captionText.innerHTML = this.alt;
+  }
+}
+
+var span = document.getElementsByClassName("close")[0];
+
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+  
+
